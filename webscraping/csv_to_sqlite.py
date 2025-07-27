ddb7e260a9f3a2d13401d2cd7c2a5fa92f4d1a91
anirudhVASUDEV23 <anirudh.vasudev.kesana@gmail.com>
2025-07-27 11:06:58 +0530

    generating db inside app and removing resources from .gitignore

--- webscraping/csv_to_sqlite.py
+++ webscraping/csv_to_sqlite.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import sqlite3
-
+import os
 # Database and CSV file paths
-db_path = 'db.sqlite'
+db_folder=os.path.join(os.path.dirname(__file__),'..','app')
+db_path =os.path.join(db_folder, 'db.sqlite')
+# Ensure the 'app' directory exists
+os.makedirs(db_folder, exist_ok=True)
 csv_path = 'flipkart_product_data.csv'
 
 # Connect to SQLite database (creates one if not exists)
